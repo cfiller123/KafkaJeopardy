ed53633247d6a53f4705c9ef10ffe4b8c445ed1f
CFillerDBS <carl.filler@daugherty.com>
2018-10-30 16:15:19 -0500

    fixed to actually jeopardy rules

--- main.py
+++ main.py
@@ -6,22 +6,29 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://put-the-db-login-path-here'
 # app.config['SQLALCHEMY_ECHO'] = True
 # db = SQLAlchemy(app)
-# app.secret_key = 'topsecretkey'
+app.secret_key = 'topsecretkey'
 
-answer = "Jeopardy"
-alphabet = 'abcdefghijklmnopqrstuvwxyz'
-alphabet_remaining = ''
+topics = []
+points = [100, 200, 300, 400, 500]
+score = 0
+
 
 @app.route('/', methods=['GET'])
 def index():
-    return render_template('index.html')
+        return render_template('index.html')
 
 @app.route('/jeopardy', methods=['GET','POST'])
 def game():
-    if request.method == 'POST':
-        guess_letter = request.form['guess_letter']
-        guess_word = request.form['guess_word']
-        if (not guess_letter) and (not guess_word):
-            flash('Please either guess a word or a letter')
+        if request.method == 'POST':
+                answer = request.form['question']
+                if (not answer):
+                        flash('Please enter something')
+                else:
+                        if (answer == correct_answer):
+                                score += 1
+                                return redirect('jeopardy.html', current_score = score)
+                        else:
+                                flash('You already entered this letter')
+        else:
+                return render_template('jeopardy.html', current_score = 0)
 
-    return render_template('jeopardy.html')
