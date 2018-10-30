10937c70cb6e772bbe5421cfe9dc57c76dc0f119
CFillerDBS <carl.filler@daugherty.com>
2018-10-30 10:20:02 -0500

    render template correctly

--- main.py
+++ main.py
@@ -1,7 +1,14 @@
-from flask import Flask
+from flask import Flask, request, redirect, render_template, flash, session
+# from flask_sqlalchemy import SQLAlchemy
+
 app = Flask(__name__)
+# app.config['Debug'] = True
+# app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://put-the-db-login-path-here'
+# app.config['SQLALCHEMY_ECHO'] = True
+# db = SQLAlchemy(app)
+# app.secret_key = 'topsecretkey'
 
 
-@app.route('/')
+@app.route('/', methods=['GET'])
 def index():
-    return index.html
+    return render_template('index.html')
