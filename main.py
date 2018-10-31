22a64315b40b4e12947feb6d579d79bacc80f9fa
CFillerDBS <carl.filler@daugherty.com>
2018-10-31 09:03:11 -0500

    game works with test answer

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, request, redirect, render_template, flash
+from flask import Flask, request, redirect, render_template, flash, session
 # from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
@@ -6,27 +6,30 @@ app = Flask(__name__)
 # app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://put-the-db-login-path-here'
 # app.config['SQLALCHEMY_ECHO'] = True
 # db = SQLAlchemy(app)
+app.secret_key='topsecretkey'
 
 topics = []
 points = [100, 200, 300, 400, 500]
-score = 0
+correct_answer = 'test'
 
 
 @app.route('/', methods=['GET'])
 def index():
-        return render_template('index.html')
+    session['score'] = 0
+    return render_template('index.html')
 
 @app.route('/jeopardy', methods=['GET','POST'])
 def game():
-        if request.method == 'POST':
-                answer = request.form['question']
-                if (not answer):
-                        flash('Please enter something')
-                else:
-                        if (answer == correct_answer):
-                                score += 1
-                                return redirect('jeopardy.html', current_score = score)
-                        else:
-                                flash('You already entered this letter')
+    new_score = session['score']
+    if request.method == 'POST':
+        answer = request.form['answer']
+        if (not answer):
+            flash('Please enter something')
         else:
-                return render_template('jeopardy.html', current_score = 0)
+            if (answer == correct_answer):
+                new_score += 1
+                session['score'] = new_score
+                return render_template('jeopardy.html', current_score = new_score)
+            else:
+                flash('Incorrect')
+    return render_template('jeopardy.html', current_score = session['score'])
