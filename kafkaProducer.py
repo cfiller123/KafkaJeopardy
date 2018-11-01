97ce74b0622fd6f7b11f6a2b0f00ec7b6d973b05
CFillerDBS <carl.filler@daugherty.com>
2018-11-01 16:24:25 -0500

    refractered files to put everything in main. started createing kafka consuming/producer

--- kafkaProducer.py
+++
@@ -1,5 +0,0 @@
-import kafka
-
-def publish_message(producer_instance, topic_name, key, value):
-    key_bytes = bytes(key, encoding='utf-8')
-    value_bytes = 
--- main.py
+++ main.py
@@ -3,7 +3,9 @@ import json
 import random
 from flask_sqlalchemy import SQLAlchemy
 from fuzzywuzzy import fuzz, process
+import kafka
 
+# Sets application configurations
 app = Flask(__name__)
 app.config['Debug'] = True
 app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://kafkaJeopardy:password@localhost:8889/kafkajeopardy'
@@ -11,6 +13,7 @@ app.config['SQLALCHEMY_ECHO'] = True
 db = SQLAlchemy(app)
 app.secret_key='topsecretkey'
 
+# User object for database
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(120))
@@ -19,12 +22,14 @@ class User(db.Model):
         self.name = name
         self.score = 0
 
+# Requires login to play game 
 @app.before_request
 def require_user():
     allowed_routes = ['index']
     if request.endpoint not in allowed_routes and 'user' not in session:
         return redirect('/')
 
+# Landing page where you "login" by entering your name
 @app.route('/', methods=['GET', 'POST'])
 def index():
     session['score'] = 0
@@ -45,6 +50,7 @@ def index():
             return redirect('/jeopardy')
     return render_template('index.html')
 
+# Main game page - updates score based on user's answers
 @app.route('/jeopardy', methods=['GET','POST'])
 def game():
     if request.method == 'POST':
@@ -65,6 +71,7 @@ def game():
     current_user = User.query.filter_by(name = session['user']).first()
     return render_template('jeopardy.html', current_score = current_user.score, question = question_selector(), name = session['user'])
 
+# Chooses a question and answer from json file
 def question_selector():
     with open('JEOPARDY_QUESTIONS1.json') as file:
         data = json.load(file)
@@ -72,6 +79,7 @@ def question_selector():
     session['answer'] = data[x]['answer']
     return data[x]['question']
 
+# Allows for 85% or better match of answer in case of misspelling/capitalization errors by user
 def fuzzy_match(guess, answer, acceptable_match):
     match = fuzz.ratio(guess,answer)
     if (match>=acceptable_match):
@@ -79,10 +87,38 @@ def fuzzy_match(guess, answer, acceptable_match):
     else:
         return False
 
+# Creates messages to push to kafka
+def publish_message(producer_instance, topic_name, key, value):
+    try:
+        key_bytes = bytes(key, encoding='utf-8')
+        value_bytes = bytes(value, encoding='utf-8')
+        producer_instance.send(topic_name, key=key_bytes, value=value_bytes)
+        producer_instance.flush()
+        print('Message published successfully.')
+    except Exception as ex:
+        print('Exception in publishing message')
+        print(str(ex))
+
+# Creates instance of kafka producer for pushing new questions
+def connect_kafka_producer():
+    _producer = None
+    try:
+        _producer = KafkaProducer(bootstrap_servers=['localhost:9092'], api_version=(0, 10))
+    except Exception as ex:
+        print('Exception while connecting Kafka')
+        print(str(ex))
+    finally:
+        return _producer
+
+# Logs out so someone else can play with their own score
 @app.route('/logout')
 def logout():
     del session['user']
     return redirect('/')
     
 if __name__ == '__main__':
+    # headers = {
+    #     'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/66.0.3359.181 Safari/537.36',
+    #     'Pragma': 'no-cache'
+    # }
     app.run()
