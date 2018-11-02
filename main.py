1c4db4106392e16f7ebe73b7e01a2e56901cdba9
CFillerDBS <carl.filler@daugherty.com>
2018-11-02 14:38:31 -0500

    kafka question queue up and running but buggy. Issues with it only finding a question the first time.

--- main.py
+++ main.py
@@ -62,22 +62,22 @@ def game():
                 current_user = User.query.filter_by(name = session['user']).first()
                 current_user.score = current_user.score + 1
                 db.session.commit()
-                return render_template('jeopardy.html', current_score = current_user.score, question = question_selector())
+                return render_template('jeopardy.html', current_score = current_user.score, question = get_kafka_question(), name = session['user'])
             else:
                 flash('Incorrect')
                 current_user = User.query.filter_by(name = session['user']).first()
                 current_user.score = current_user.score - 1
                 db.session.commit()
     current_user = User.query.filter_by(name = session['user']).first()
-    return render_template('jeopardy.html', current_score = current_user.score, question = question_selector(), name = session['user'])
+    return render_template('jeopardy.html', current_score = current_user.score, question = get_kafka_question(), name = session['user'])
 
 # Chooses a question and answer from json file
-def question_selector():
-    with open('JEOPARDY_QUESTIONS1.json') as file:
-        data = json.load(file)
-    x = random.randint(1,501)
-    session['answer'] = data[x]['answer']
-    return data[x]['question']
+# def question_selector():
+#     with open('JEOPARDY_QUESTIONS1.json') as file:
+#         data = json.load(file)
+#     x = random.randint(1,501)
+#     session['answer'] = data[x]['answer']
+#     return data[x]['question']
 
 # Allows for 85% or better match of answer in case of misspelling/capitalization errors by user
 def fuzzy_match(guess, answer, acceptable_match):
@@ -94,7 +94,9 @@ def get_kafka_question():
     servers = ['localhost:9092']
     consumer = KafkaConsumer(topic, group_id = group_id, bootstrap_servers=servers, auto_offset_reset='earliest', consumer_timeout_ms=3000)
     for msg in consumer:
-        return parse_message(msg.value)
+        temp = parse_message(msg.value)
+        session['answer'] = temp['answer']
+        return temp['question']
         break
     consumer.close()
 
